d2a81a9f9b0c0f17e40d1e71b8b562434a56f093
halimah <hanahussein6155@gmail.com>
2022-12-20 15:28:55 +0300

    commited

--- backend/src/api.py
+++ backend/src/api.py
@@ -18,8 +18,8 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-with app.app_context():
-     db_drop_and_create_all()
+# with app.app_context():
+#      db_drop_and_create_all()
 
 # ROUTES
 '''
